952c02d2dd3e6716d7302f04c6dac1f136544d3a
Sazad Ahemad <mail2sazad@gmail.com>
2023-05-08 00:16:35 +0530

    updated css

--- profile.php
+++ profile.php
@@ -1,84 +1,86 @@
 <html>
-    <head>
-        <title> student profile</title>
-        <link rel="stylesheet"href="my.css" type="text/css">
-    </head>
-    <body>
-       <div class="main">
-        <div class="register">
-        <form>
-            <div>
-            Name:<input type="text">
-            </div>
-            <br>
-           <div>
-           fathers name:<input type="text">
-           </div>
-           <br>
-           <div>
-           mothers name:<input type="text">
-           </div>
-           <br>
-           <div>
-           phone no:<input type="text">
-           </div><br>
-           <div>
-            email:<input type="email">
-           </div>
-           <br>
-       <div>
-          Gender female:<input type="radio"name="my gender">
-         male:<input type="radio"name="my gender">
-         other:<input type="radio"name="my gender">
-       </div> 
-         <br>  
-         <div>
-            Date of Birth:<input type="date">
-        
-         </div> 
-         <br>
-         <div>
-            Adress:<input type="text">
-         </div>
-            <br>
-            <div>
-                Branch
-                <select name="drop-down">
-                    <option value="csit">cse</option>
-                    <option value="cst">cst</option>
-                    <option value="cse">csit</option>
-                    <option value="ece">ece</option>
-                    <option value="civil">civil</option>
-                </select>
 
-            </div>
-            <br>
-            <div>
-                state
-                <select name="drop down">
-                    <option value="odisha">punjab</option>
-                    <option value="punjab">odisha</option>
-                    <option value="karnatak">karnatak</option>
-                    <option value="rajastan">rajasthan</option>
-                </select>
-            </div>
-            <br>
-            <div>
-               COURSESBtech<input type="check box">
-               mtech<input type="check box">
-              MBA<input type="check box">
-              MCA<input type="check box">
-            </div>
-            <br>
-            <div>
-                Image:<input type="file">
-            </div>
-            <br>
-            <div>
-                <input type="submit"value="submit">
-            </div>
- </form>
+<head>
+    <title> student profile</title>
+    <link rel="stylesheet" href="my.css" type="text/css">
+</head>
+
+<body>
+    <div class="main">
+        <div class="register">
+            <form>
+                <div class="input-control">
+                    <label for="name">Name</label>
+                    <input id="name" type="text">
+                </div>
+                <div class="input-control">
+                    <label for="name">Father's Name</label>
+                    <input type="text">
+                </div>
+                <div class="input-control">
+                    <label for="name">mother's Name</label>
+                    <input type="text">
+                </div>
+                <div class="input-control">
+                    <label for="name">phone no</label>
+                    <input type="number">
+                </div>
+                <div class="input-control">
+                    <label for="name">email</label>
+                    <input type="email">
+                </div>
+                <div class="input-control">
+                    <label for="name">Gender</label><br>
+                    female:<input type="radio" name="gender">
+                    male:<input type="radio" name="gender">
+                    other:<input type="radio" name="gender">
+                </div>
+                <div class="input-control">
+                <label for="name">DOB</label>
+                    <input type="date">
+                </div>
+                <div  class="input-control">
+                <label for="name">Address</label>    
+                <input type="text">
+                </div>
+                <div class="input-control">
+                    <label for="name">Branch</label>
+                    <select name="drop-down">
+                        <option value="csit">cse</option>
+                        <option value="cst">cst</option>
+                        <option value="cse">csit</option>
+                        <option value="ece">ece</option>
+                        <option value="civil">civil</option>
+                    </select>
+                </div>
+                <div class="input-control">
+                    <label for="name">State</label>
+                    <select name="drop down">
+                        <option value="odisha">punjab</option>
+                        <option value="punjab">odisha</option>
+                        <option value="karnatak">karnatak</option>
+                        <option value="rajastan">rajasthan</option>
+                    </select>
+                </div>
+                <div class="input-control">
+                    <label for="name">Course</label>
+                    <select name="drop down">
+                        <option value="B. Tech">B. Tech</option>
+                        <option value="M. Tech">M. Tech</option>
+                        <option value="MBA">MBA</option>
+                        <option value="MCA">MCA</option>
+                    </select>
+                </div>
+                <div class="input-control">
+                    <label for="name">Image</label>
+                    <input type="file">
+                </div>
+                <div class="input-control">
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
     </div>
-       </div>
 </body>
-    </html>
+
+</html>
