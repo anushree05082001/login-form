73ccd9bdf4ad13b4ff9781018a446cf59900a2a1
anushree05082001 <paridaanushree32@gmail.com>
2023-05-07 10:52:52 -0700

    Added Profile Details

---
+++ profile.php
@@ -0,0 +1,84 @@
+<html>
+    <head>
+        <title> student profile</title>
+        <link rel="stylesheet"href="my.css" type="text/css">
+    </head>
+    <body>
+       <div class="main">
+        <div class="register">
+        <form>
+            <div>
+            Name:<input type="text">
+            </div>
+            <br>
+           <div>
+           fathers name:<input type="text">
+           </div>
+           <br>
+           <div>
+           mothers name:<input type="text">
+           </div>
+           <br>
+           <div>
+           phone no:<input type="text">
+           </div><br>
+           <div>
+            email:<input type="email">
+           </div>
+           <br>
+       <div>
+          Gender female:<input type="radio"name="my gender">
+         male:<input type="radio"name="my gender">
+         other:<input type="radio"name="my gender">
+       </div> 
+         <br>  
+         <div>
+            Date of Birth:<input type="date">
+        
+         </div> 
+         <br>
+         <div>
+            Adress:<input type="text">
+         </div>
+            <br>
+            <div>
+                Branch
+                <select name="drop-down">
+                    <option value="csit">cse</option>
+                    <option value="cst">cst</option>
+                    <option value="cse">csit</option>
+                    <option value="ece">ece</option>
+                    <option value="civil">civil</option>
+                </select>
+
+            </div>
+            <br>
+            <div>
+                state
+                <select name="drop down">
+                    <option value="odisha">punjab</option>
+                    <option value="punjab">odisha</option>
+                    <option value="karnatak">karnatak</option>
+                    <option value="rajastan">rajasthan</option>
+                </select>
+            </div>
+            <br>
+            <div>
+               COURSESBtech<input type="check box">
+               mtech<input type="check box">
+              MBA<input type="check box">
+              MCA<input type="check box">
+            </div>
+            <br>
+            <div>
+                Image:<input type="file">
+            </div>
+            <br>
+            <div>
+                <input type="submit"value="submit">
+            </div>
+ </form>
+    </div>
+       </div>
+</body>
+    </html>
